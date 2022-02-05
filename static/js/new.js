dbd64c3560784a2319d237e227ab43d7803c6eca
George Tsitsiridis <gtsitsiridis@gmail.com>
2022-02-06 01:13:19 +0200

    Only save if content has changed

--- static/js/new.js
+++ static/js/new.js
@@ -2,6 +2,7 @@ const UPLOAD_PATH = document.currentScript.getAttribute('upload_path');
 const AUTOSAVE_INTERVAL = 5000
 let pageName = window.location.pathname
 pageName = pageName.substring(pageName.indexOf('/', 1) + 1)
+let oldContent = null
 
 function copy_fun(mes) {
     var tempInput = document.createElement("input");
@@ -63,44 +64,50 @@ function validateForm() {
 }
 
 function save(name, redirect = false, auto = false) {
-    let url = `/${window.location.pathname.split('/')[1]}`;
-    if (url === '/edit') {
-        url = url + "/" + pageName
-    }
-    axios({
-        method: 'post',
-        url: url,
-        data: {
-            name: name,
-            content: editor.getValue()
-        }
-    }).then((response) => {
-        if (redirect) {
-            window.location.replace(`/${name}`)
-        }
-        pageName = name
-        const today = new Date();
-        var dt = today.toLocaleDateString("en-GB", { // you can use undefined as first argument
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-        });
-        let savetimeElement = document.getElementById("save-time")
-        savetimeElement.innerHTML = `Last saved: ${dt}`;
-        savetimeElement.hidden = false;
+    const content = editor.getValue()
+    const hasChanged = oldContent !== content || name !== pageName
 
-        if (auto && document.getElementById("autosave").checked) {
-            this.autosaveTimeout = setTimeout(autosave, AUTOSAVE_INTERVAL);
+    if (hasChanged) {
+        let url = `/${window.location.pathname.split('/')[1]}`;
+        if (url === '/edit') {
+            url = url + "/" + pageName
         }
-    }, (error) => {
-        console.log(error);
-        if (auto && document.getElementById("autosave").checked) {
-            this.autosaveTimeout = setTimeout(autosave, AUTOSAVE_INTERVAL);
-        }
-    });
+        axios({
+            method: 'post',
+            url: url,
+            data: {
+                name: name,
+                content: content
+            }
+        }).then((response) => {
+            if (redirect) {
+                window.location.replace(`/${name}`)
+            }
+            pageName = name
+            oldContent = content
+            const today = new Date();
+            var dt = today.toLocaleDateString("en-GB", { // you can use undefined as first argument
+                year: "numeric",
+                month: "2-digit",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            });
+            let savetimeElement = document.getElementById("save-time")
+            savetimeElement.innerHTML = `Last saved: ${dt}`;
+            savetimeElement.hidden = false;
+
+            if (auto && document.getElementById("autosave").checked) {
+                this.autosaveTimeout = setTimeout(autosave, AUTOSAVE_INTERVAL);
+            }
+        }, (error) => {
+            console.log(error);
+            if (auto && document.getElementById("autosave").checked) {
+                this.autosaveTimeout = setTimeout(autosave, AUTOSAVE_INTERVAL);
+            }
+        });
+    }
 }
 
 // This method will autosave the page every x seconds
